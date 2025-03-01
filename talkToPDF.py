3e0bebfab10588f968940b2bbd5993fa8b8e66fd
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-03-01 14:43:50 +0530

    Update talkToPDF.py

--- talkToPDF.py
+++ talkToPDF.py
@@ -37,7 +37,7 @@ def vector_store(text_chunks):
 
 def get_conversational_chain(tools,ques):
     #os.environ["ANTHROPIC_API_KEY"]=os.getenv["ANTHROPIC_API_KEY"]
-    llm = ChatAnthropic(model="Claude 3.7 Sonnet", temperature=0, api_key=os.getenv("ANTHROPIC_API_KEY"),verbose=True)
+    llm = ChatAnthropic(model="claude-3-haiku-20240307", temperature=0, api_key=os.getenv("ANTHROPIC_API_KEY"),verbose=True)
     #llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
     prompt = ChatPromptTemplate.from_messages(
     [
